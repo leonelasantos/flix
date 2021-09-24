e81589643b74b5babebfad81f8b33de6a54d8d69
Leonel Santos <leonel.santos.a@gmail.com>
2021-09-24 17:44:08 -0400

    completed Flix

---
+++ Flix/MovieGridCell.swift
@@ -0,0 +1,13 @@
+//
+//  MovieGridCell.swift
+//  Flix
+//
+//  Created by Leonel Santos on 9/24/21.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    
+    @IBOutlet weak var posterView: UIImageView!
+}
